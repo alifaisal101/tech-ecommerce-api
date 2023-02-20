3d00a9b30a76686d2a38cde83870bb09fbc819e0
Ali <alifais1122@gmail.com>
2023-02-20 04:49:46 +0300

    implemented drive Dto

--- src/drives/dtos/drives.dto.ts
+++ src/drives/dtos/drives.dto.ts
@@ -1,1 +1,31 @@
-export class DrivesDto {}
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+} from 'class-validator';
+
+export class DrivesDto {
+  @IsNumber()
+  @IsPositive()
+  writeSpeed: number;
+
+  @IsNumber()
+  @IsPositive()
+  readSpeed: number;
+
+  @IsString()
+  @IsNotEmpty()
+  type: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  rpm?: number;
+
+  @IsBoolean()
+  @IsOptional()
+  nvme?: boolean;
+}
